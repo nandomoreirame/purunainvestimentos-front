9573bb6c4d7c2771d0409b3a058d9998434ef8ea
Fernando Moreira <nandomoreira.me@gmail.com>
2019-07-27 12:57:32 -0300

    loading template

---
+++ components/Loading.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="loading" class="loading-page">
+    <p>Loading...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    loading: false
+  }),
+  methods: {
+    start () {
+      this.loading = true
+    },
+    finish () {
+      this.loading = false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.loading-page {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #f8f8f8;
+  text-align: center;
+  padding-top: 20px;
+  font-size: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 100000;
+}
+</style>
